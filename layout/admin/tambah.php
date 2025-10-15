b1939f8ff4c423b67a482794b890659332741b8a
rizkyy20 <rizkyfile12@gmail.com>
2025-10-15 23:26:36 +0800

    Styling tampila tambah data

--- layout/admin/tambah.php
+++ layout/admin/tambah.php
@@ -23,49 +23,55 @@ if(isset($_POST['tambah'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
     <title>Tambah Data</title>
 </head>
 <body>
-    <form action="" method="post">
-        <div>
-            <label for="">id</label>
-            <div>
-                <input type="number" name="id_tamu">
-            </div>
+    <div class="d-flex justify-content-center min-vh-100 align-items-center">
+        <div class="bg-primary p-4 rounded text-light fw-semibold">
+            <form action="" method="post">
+                <div>
+                    <label for="">Id</label>
+                    <div>
+                        <input type="number" name="id_tamu" class="form-control">
+                    </div>
+                </div>
+                <div>
+                    <label for="">Nama</label>
+                    <div>
+                        <input type="text" name="nama" class="form-control">
+                    </div>
+                </div>
+                <div>
+                    <label for="">alamat</label>
+                    <div>
+                        <input type="text" name="alamat" class="form-control">
+                    </div>
+                </div>
+                <div>
+                    <label for="">email</label>
+                    <div>
+                        <input type="email" name="email" class="form-control">
+                    </div>
+                </div>
+                <div>
+                    <label for="">organisasi</label>
+                    <div>
+                        <input type="text" name="organisasi" class="form-control">
+                    </div>
+                </div>
+                <div>
+                    <label for="">pesan</label>
+                    <div>
+                        <input type="text" name="pesan" class="form-control">
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center mt-3">
+                    <button class="btn btn-light fw-semibold" name="tambah" type="submit">Tambah</button>
+                </div>
+            </form>
         </div>
-        <div>
-            <label for="">Nama</label>
-            <div>
-                <input type="text" name="nama">
-            </div>
-        </div>
-        <div>
-            <label for="">alamat</label>
-            <div>
-                <input type="text" name="alamat">
-            </div>
-        </div>
-        <div>
-            <label for="">email</label>
-            <div>
-                <input type="email" name="email">
-            </div>
-        </div>
-        <div>
-            <label for="">organisasi</label>
-            <div>
-                <input type="text" name="organisasi">
-            </div>
-        </div>
-        <div>
-            <label for="">pesan</label>
-            <div>
-                <input type="text" name="pesan">
-            </div>
-        </div>
-        <div>
-            <button name="tambah" type="submit">Tambah</button>
-        </div>
-    </form>
+    </div>
 </body>
 </html>
